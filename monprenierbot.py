7c03d96bc90a2d7eeac0ec623f6854689a691437
ballakeita <ballakei070@gmail.com>
2020-02-06 11:06:32 +0100

    mon premier bot

---
+++ monprenierbot.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+#salutation = ['massil', 'ahmim', 'insim', 'bachelor', 'dev', 'web', 'python', 'juv', 'azul', 'amazigh', 'bay']
+question = input("bonjour, je suis votre bot, veuillez donner un mot")
+
+
+
+if (question.lower() == "salut"):
+    print("salut mon ami")
+elif (question.lower() == "bonjour"):
+    print("salut mon ami")
+
