0b21b3e017af6976fa54c49d08c26ff5149dafcd
ballakeita <ballakei070@gmail.com>
2020-02-06 11:42:25 +0100

    version 2 du bot

--- monprenierbot.py
+++ monprenierbot.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 #salutation = ['massil', 'ahmim', 'insim', 'bachelor', 'dev', 'web', 'python', 'juv', 'azul', 'amazigh', 'bay']
-question = input("bonjour, je suis votre bot, veuillez donner un mot")
+question = input("bonjour, je suis votre bot, veuillez donner un mot\n")
 
+proposition = ["salut","bonjour","bonsoir"]
 
-
-if (question.lower() == "salut"):
+if (question.lower() in proposition):
     print("salut mon ami")
-elif (question.lower() == "bonjour"):
-    print("salut mon ami")
+else:
+    print("je ne comprend pas")
 
